b73c2108c5b6dbccd17df6d7500b55e04afffb8f
Mateo <mateobarac0712@gmx.de>
2025-04-25 10:20:07 +0200

    display.cpp added, display_tests works

--- src/models/displays/display.cpp
+++ src/models/displays/display.cpp
@@ -14,19 +14,18 @@ Display::Display(std::string const& initialText)
 
 void Display::updateText(std::string const& newText)
 {
-    // TODO
+    Display::text = newText;
 }
 
 std::string Display::getText() const
 {
-    // TODO
-
-    return "";
+    //gibt den Text eines Objekts des Typs Display zurück
+    return Display::text;
 }
 
 void Display::show() const
 {
-    // TODO
+    std::cout << Display::getText();
 
     // Geben Sie das Ergebnis von `getText()` auf der Konsole aus.
 
